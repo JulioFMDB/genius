62cccb763eab7552ab99f8096b62f6ecf4577f52
JulioFMDB <33849308+JulioFMDB@users.noreply.github.com>
2022-07-22 17:18:17 -0300

    Update GeniusFinal.cpp
    removi o comentario q tava na linha 10

--- GeniusFinal.cpp
+++ GeniusFinal.cpp
@@ -7,7 +7,6 @@
 //#include <conio2.h>
 #include <windows.h>
 #include "conio.c"
-// sdfsldfhjdsh
 /***********************************************/
 /* Definição das Estruturas                    */
 /***********************************************/
